009bd94005b8945dd8491352c410646a94682ae0
Jose Jachuf <jjachuf@gmail.com>
2024-01-03 09:46:10 -0300

    Some of the code was moved to a new module, html.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::{Arg, Command};
 use salvo::prelude::*;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 // use dirs;
 
+mod html;
 mod handlers;
 mod utils;
 
@@ -10,75 +11,6 @@ async fn init(data_dir: &str) {
     if !Path::new(data_dir).exists() {
         std::fs::create_dir(&data_dir).unwrap();
     }
-}
-
-#[handler]
-async fn index(res: &mut Response) -> Result<(), anyhow::Error> {
-    let data_dir = "data";
-
-    let mut data_content = tokio::fs::read_dir(data_dir).await?;
-    let mut data_files: Vec<PathBuf> = Vec::new();
-
-    while let Some(data_input) = data_content.next_entry().await? {
-        let ruta = data_input.path();
-        let name = data_input.file_name();
-        if ruta.is_file() {
-            data_files.push(name.into());
-        }
-    }
-
-    let data_files: Vec<String> = data_files
-        .into_iter()
-        .filter_map(|path| {
-            path.file_stem().map(|stem| {
-                format!(
-                    r#"<li><a href="/api/{}">{}</a></li>"#,
-                    stem.to_string_lossy().to_string(),
-                    stem.to_string_lossy().to_string()
-                )
-            })
-        })
-        .collect();
-
-    let html = format!(
-        r#"
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>Static API</title>
-            </head>
-            <body>
-                <h1>Static API</h1>
-
-                <p>This is a simple application simulating a basic REST API. It allows CRUD operations (Create, Read, Update, Delete) on different collections, where each collection is represented as a JSON file in the file system. If the collection does not exist, it is automatically created.</p>
-
-                <h3>Collections</h3>
-
-                <ul>{}</ul>
-
-                <h3>Try something like</h3>
-
-                <div style="background-color: #DEDEDE; padding: 10px;">
-
-                    <pre>curl -X GET http://localhost:5800/api/&lt;collection&gt;</pre>
-
-                    <pre>curl -X GET http://localhost:5800/api/&lt;collection&gt;/&lt;id&gt;</pre>
-
-                    <pre>curl -X POST -H "Content-Type: application/json" -d '{{"field1":"value1", "field2":"value2"}}' http://localhost:5800/api/&lt;collection&gt;</pre>
-
-                    <pre>curl -X PUT -H "Content-Type: application/json" -d '{{"field1":"new_value1", "new_field2":"value2"}}' http://localhost:5800/api/&lt;collection&gt;/&lt;id&gt;</pre>
-
-                    <pre>curl -X DELETE http://localhost:5800/api/&lt;collection&gt;/&lt;id&gt;</pre>
-                </div>
-
-            </body>
-        </html>
-        "#,
-        data_files.join("")
-    );
-
-    res.render(Text::Html(html));
-    Ok(())
 }
 
 #[tokio::main]
@@ -120,7 +52,7 @@ async fn main() {
     init("data").await;
 
     let router = Router::new()
-        .get(index)
+        .get(html::index)
         .push(
             Router::with_path("api/<f>")
                 .get(handlers::get_all)
